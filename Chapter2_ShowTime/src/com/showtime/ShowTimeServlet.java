b005496f27a7f5e79f4b26b7580df6432d35d9f4
pkx0128 <764670547@qq.com>
2022-06-08 23:18:10 +0800

    实现页面显示动态时间

---
+++ Chapter2_ShowTime/src/com/showtime/ShowTimeServlet.java
@@ -0,0 +1,49 @@
+package com.showtime;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowTimeServlet
+ */
+@WebServlet("/ShowTimeServlet")
+public class ShowTimeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ShowTimeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		response.setHeader("Refresh", "1");
+		SimpleDateFormat dfm = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+		
+		response.getWriter().println("<h1>现在时间这：" + dfm.format(new Date()) +"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
